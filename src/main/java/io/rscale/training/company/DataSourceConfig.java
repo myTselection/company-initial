94d06a701791f636a2cda7d2a5ab7d13dbad9988
myTselection <#yrQaMFfr6U$JQ5TsVkpzLAGB>
2017-10-19 13:47:53 +0200

    working company app using config server via config-service

--- src/main/java/io/rscale/training/company/DataSourceConfig.java
+++ src/main/java/io/rscale/training/company/DataSourceConfig.java
@@ -18,22 +18,7 @@ import java.sql.SQLException;
 public class DataSourceConfig extends AbstractCloudConfig {
 
     private static final Logger logger = Logger.getLogger(DataSourceConfig.class);
-    @Value("${mysql.dbName}")
-    private String mysqlDbName;
 
-    @Value("${mysql.user}")
-    private String mysqlUser;
-
-    @Value("${mysql.password}")
-    private String mysqlPassword;
-
-    @Value("${mysql.server}")
-    private String mysqlServer;
-
-    @Value("${mysql.port}")
-    private int mysqlPort;
-
-    private DataSource dataSource;
     public DataSourceConfig() {
         logger.info(this.getClass() + " loaded");
     }
@@ -51,24 +36,6 @@ public class DataSourceConfig extends AbstractCloudConfig {
     private boolean isMySQL(DataSource dataSource) throws SQLException {
         logger.info("datasource drivername information retrieved: " + dataSource.getConnection().getMetaData().getDriverName());
         return dataSource.getConnection().getMetaData().getDriverName().toLowerCase().contains("mysql");
-    }
-
-    @PostConstruct
-    public void init() {
-        logger.info(this.getClass() + " loaded");
-        try {
-            MysqlDataSource mysql = new MysqlDataSource();
-            mysql.setDatabaseName(mysqlDbName);
-            mysql.setUser(mysqlUser);
-            mysql.setPassword(mysqlPassword);
-            mysql.setServerName(mysqlServer);
-            mysql.setPort(mysqlPort);
-            logger.info("Created datasource:  " + mysql);
-            this.dataSource = mysql;
-        } catch (Exception e) {
-            logger.error("Could not create datasoruce", e);
-        }
-        logger.info(this.getClass() + " initialized");
     }
 
 }
